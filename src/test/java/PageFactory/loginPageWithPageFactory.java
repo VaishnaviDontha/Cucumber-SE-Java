cf22e6227186d2958bf84ad9e58e0e31bea9dcf7
VaishnaviDontha <vaishnavidontha24@gmail.com>
2022-07-05 16:46:39 -0400

    PageFactory Example

--- src/test/java/PageFactory/loginPageWithPageFactory.java
+++ src/test/java/PageFactory/loginPageWithPageFactory.java
@@ -1,2 +1,40 @@
-package PageFactory;public class loginPageWithPageFactory {
+package PageFactory;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class loginPageWithPageFactory {
+
+    protected WebDriver dr;
+
+    public loginPageWithPageFactory(WebDriver driver){
+
+        this.dr = driver;
+        PageFactory.initElements(driver, loginPageWithPageFactory.class);
+
+    }
+
+    @FindBy(id = "txtUsername")
+    WebElement txtUsername;
+
+    @FindBy(id = "txtPassword")
+    WebElement txtPassword;
+
+    @FindBy(id = "btnLogin")
+    WebElement login;
+
+
+    public void enterUserName(String username){
+        txtUsername.sendKeys(username);
+    }
+
+    public void enterPassword(String password){
+        txtPassword.sendKeys(password);
+    }
+
+    public void clickLogin(){
+        login.click();
+    }
 }
